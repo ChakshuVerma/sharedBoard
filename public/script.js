e92107eeb0f602e50ae944f7c66b0650f448efc2
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-05-12 12:49:59 +0530

    updated canvas

--- public/script.js
+++ public/script.js
@@ -1,81 +1,90 @@
-let socket = io(),
-    ctx,
-    colour,
-    weight = "5",
-    timeout,
-    canvas;
+let socket = io();
+let canvas, ctx, color, weight, isDrawing, timeout;
 
+// This gives the Initial State To Canvas
 socket.emit('login')
 
-drawOnCanvas()
+setupCanvas();
 
-function drawOnCanvas () {
-    canvas = document.querySelector('#paint');
+// Function To Set Up Canvas Initially
+function setupCanvas() {
+    canvas = document.getElementById('paint');
+    var canvas_style = getComputedStyle(canvas);
+    canvas.width = parseInt(canvas_style.getPropertyValue('width'));
+    canvas.height = parseInt(canvas_style.getPropertyValue('height'));
     ctx = canvas.getContext('2d');
-
-    var sketch = document.querySelector('#sketch');
-    var sketch_style = getComputedStyle(sketch);
-    canvas.width = parseInt(sketch_style.getPropertyValue('width'));
-    canvas.height = parseInt(sketch_style.getPropertyValue('height'));
-
-    var mouse = {x: 0, y: 0};
-    var last_mouse = {x: 0, y: 0};
-
-    /* Mouse Capturing Work */
-    canvas.addEventListener('mousemove', function(e) {
-        last_mouse.x = mouse.x;
-        last_mouse.y = mouse.y;
-
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
-    }, false);
-
-
-    /* Drawing on Paint App */
-    ctx.lineWidth = weight;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = colour;
-
-    canvas.addEventListener('mousedown', function(e) {
-        canvas.addEventListener('mousemove', onPaint, false);
-    }, false);
-    
-    canvas.addEventListener('mouseup', function() {
-        canvas.removeEventListener('mousemove', onPaint, false);
-    }, false);
-
-    canvas.addEventListener('mouseout', function() {
-        canvas.removeEventListener('mousemove', onPaint, false);
-    }, false);
-
-    var onPaint = function() {
-        ctx.beginPath();
-        ctx.moveTo(last_mouse.x, last_mouse.y);
-        ctx.lineTo(mouse.x, mouse.y);
-        ctx.closePath();
-        ctx.stroke();
-
-        if(timeout != undefined)
-            clearTimeout(timeout)
-
-        timeout = setTimeout( () => {
-            var base64ImageData = canvas.toDataURL("image/png")
-            socket.emit('canvas-data', base64ImageData)
-        },400)
-    };
+    weight = '5';
+    color = '#000000'
 }
 
+
+// Canvas Event Listeners
+canvas.addEventListener('touchstart', start, false);
+canvas.addEventListener('touchmove', draw, false);
+canvas.addEventListener('mousedown', start, false);
+canvas.addEventListener('mousemove', draw, false);
+canvas.addEventListener('touchend', stop, false);
+canvas.addEventListener('mouseup', stop, false);
+canvas.addEventListener('mouseout', stop, false);
+
+
+// Function To Start Drawing
+function start(event) {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(event.clientX - canvas.offsetLeft,event.clientY - canvas.offsetTop);
+    event.preventDefault();
+}
+
+
+// Function To Draw
+function draw(event) {
+    if(isDrawing) {
+        ctx.lineTo(event.clientX - canvas.offsetLeft,event.clientY - canvas.offsetTop);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = weight;
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round'
+        ctx.stroke();
+    }
+    event.preventDefault();
+
+    if(timeout != undefined)
+        clearTimeout(timeout)
+
+    timeout = setTimeout( () => {
+        var base64ImageData = canvas.toDataURL("image/png")
+        socket.emit('canvas-data', base64ImageData)
+    },400)
+}
+
+
+// Function To Stop Drawing
+function stop(event) {
+    if(isDrawing) {
+        ctx.stroke();
+        ctx.closePath();
+        isDrawing = false;
+    }
+    event.preventDefault();
+}
+
+
+// Function To Change Color Of Brush
 function changecolor(c) {
-    colour = c.value;
-    ctx.strokeStyle = colour;
+    color = c.value;
+    ctx.strokeStyle = color;
  }
 
+
+// Function To Change Weight Of Brush
 function changeweight(c) {
     weight = c.value;
     ctx.lineWidth = weight;
 }
 
+
+// Function To Clear Canvas
 function clearcanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     socket.emit('clear-canvas')
